ecca29f0473117a3ae1799c82a451af9b0e84129
Rafael Custodio <rafaellcustodio@gmail.com>
2019-02-22 09:48:04 -0300

    Varios olas mundo

--- App1/Program.cs
+++ App1/Program.cs
@@ -7,6 +7,12 @@ namespace App1
         static void Main(string[] args)
         {
             Console.WriteLine("Olá mundo!!!!");
+            Console.WriteLine("Olá mundo!!!!");
+
+
+            Console.WriteLine("Olá mundo!!!!"); Console.WriteLine("Olá mundo!!!!");
+
+            Console.WriteLine("Olá mundo!!!!");
         }
     }
 }
