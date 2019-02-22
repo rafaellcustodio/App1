d618fac9371569c17b477e1f035903ccc73fc8a0
Rafael Custodio <rafaellcustodio@gmail.com>
2019-02-22 10:04:14 -0300

    mundo cruel e triste demais

--- App1/Program.cs
+++ App1/Program.cs
@@ -6,7 +6,7 @@ namespace App1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Olá mundo cruel!!!!");
+            Console.WriteLine("Olá mundo cruel e triste demais!!!!");
             
         }
     }
