2ebd163385f09e41168944485082b5e177b7827a
Rafael Custodio <rafaellcustodio@gmail.com>
2019-02-22 09:40:32 -0300

    Ola mundo em portugues

--- App1/Program.cs
+++ App1/Program.cs
@@ -6,7 +6,7 @@ namespace App1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Olá mundo!!!!");
         }
     }
 }
